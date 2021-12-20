2b88a1c35e688d8ec7dfaca182c7f35a8d7e54c5
Matheus Henrique Musa da Silva <mattheus.h.m.s@gmail.com>
2021-12-20 17:21:35 -0300

    chore: errors message

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import "express-async-errors"
 import "./database"
 import "./shared/container"
 import { router } from './routes'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './swagger.json'
+import { AppError } from './errors/error'
 
 const app = express()
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(router)
+
+app.use((e: Error, req: Request, res: Response, next: NextFunction) =>{
+  if (e instanceof AppError){
+    return res.status(e.statusCode).json({
+      message: e.message
+    })
+  }
+  return res.status(500).json({
+    message: 'Internal Server Error'
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
